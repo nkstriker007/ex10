ad502492da9434286b17b2b349cbd872c6ced266
Narayan Krishna <narayankrishna.tvr@gmail.com>
2024-11-07 18:15:50 +0530

    message

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! Welcome to my Dockerized Flask app by RNK"
+    return "Hello, World! Welcome to my Dockerized Flask App by RNK"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
